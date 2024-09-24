fff1195ccbea8c0887d6223a50f26e7c5e08d7d1
sbobruskina <sbobruskina@pyrus.com>
2024-09-24 17:34:38 +0300

    add bridging-header

---
+++ Te/Te-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  Te-Bridging-Header.h
+//  Te
+//
+//  Created by Станислава Бобрускина on 24.09.2024.
+//
+
+#ifndef Te_Bridging_Header_h
+#define Te_Bridging_Header_h
+
+#import <UIKit/UIKit.h>
+
+#endif /* Te_Bridging_Header_h */
